0966887fe64b9b98634a24d7d0340c2f31a3419a
St3eT <St3eT@seznam.cz>
2015-06-13 12:28:26 +0200

    Quest Failure and its Consequences (10392).

--- l2junity-gameserver/dist/game/data/scripts/quests/Q10392_FailureAndItsConsequences/Q10392_FailureAndItsConsequences.java
+++ l2junity-gameserver/dist/game/data/scripts/quests/Q10392_FailureAndItsConsequences/Q10392_FailureAndItsConsequences.java
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-package quests.Q10391_ASuspiciousHelper;
+package quests.Q10392_FailureAndItsConsequences;
 
 import org.l2junity.gameserver.enums.Race;
 import org.l2junity.gameserver.model.actor.Npc;
@@ -24,34 +24,43 @@ import org.l2junity.gameserver.model.actor.instance.PlayerInstance;
 import org.l2junity.gameserver.model.quest.Quest;
 import org.l2junity.gameserver.model.quest.QuestState;
 import org.l2junity.gameserver.model.quest.State;
+import org.l2junity.gameserver.network.client.send.string.NpcStringId;
+
+import quests.Q10391_ASuspiciousHelper.Q10391_ASuspiciousHelper;
 
 /**
- * A Suspicious Helper (10391)
+ * Failure and its Consequences (10392)
  * @author St3eT
  */
-public final class Q10391_ASuspiciousHelper extends Quest
+public final class Q10392_FailureAndItsConsequences extends Quest
 {
 	// NPCs
-	private static final int ELI = 33858;
-	private static final int CHEL = 33861;
 	private static final int IASON = 33859;
+	private static final int ELI = 33858;
+	private static final int[] MONSTERS =
+	{
+		20991, // Swamp Tribe
+		20992, // Swamp Alligator
+		20993, // Swamp Warrior
+	};
 	// Items
-	private static final int CARD = 36707; // Forged Identification Card
-	private static final int EXP_MATERTIAL = 36708; // Experimental Material
+	private static final int FRAGMENT = 36709; // Suspicious Fragment
 	private static final int STEEL_COIN = 37045; // Steel Door Guild Coin
 	private static final int EAC = 952; // Scroll: Enchant Armor (C-grade)
 	// Misc
 	private static final int MIN_LEVEL = 40;
 	private static final int MAX_LEVEL = 46;
 	
-	public Q10391_ASuspiciousHelper()
+	public Q10392_FailureAndItsConsequences()
 	{
-		super(10391, Q10391_ASuspiciousHelper.class.getSimpleName(), "A Suspicious Helper");
-		addStartNpc(ELI);
-		addTalkId(ELI, CHEL, IASON);
-		registerQuestItems(CARD, EXP_MATERTIAL);
-		addCondLevel(MIN_LEVEL, MAX_LEVEL, "33858-06.htm");
-		addCondNotRace(Race.ERTHEIA, "33858-07.htm");
+		super(10392, Q10392_FailureAndItsConsequences.class.getSimpleName(), "Failure and its Consequences");
+		addStartNpc(IASON);
+		addTalkId(IASON, ELI);
+		addKillId(MONSTERS);
+		registerQuestItems(FRAGMENT);
+		addCondNotRace(Race.ERTHEIA, "33859-10.html");
+		addCondLevel(MIN_LEVEL, MAX_LEVEL, "33859-09.htm");
+		addCondCompletedQuest(Q10391_ASuspiciousHelper.class.getSimpleName(), "33859-09.htm");
 	}
 	
 	@Override
@@ -67,41 +76,39 @@ public final class Q10391_ASuspiciousHelper extends Quest
 		String htmltext = null;
 		switch (event)
 		{
-			case "33858-02.htm":
-			case "33858-03.htm":
-			case "33861-02.html":
-			case "33859-02.html":
-			case "33859-03.html":
+			case "33859-02.htm":
+			case "33859-03.htm":
+			case "33858-02.html":
+			case "33858-03.html":
 			{
 				htmltext = event;
 				break;
 			}
-			case "33858-04.htm":
+			case "33859-04.htm":
 			{
-				giveItems(player, CARD, 1);
 				st.startQuest();
+				st.setQuestLocation(NpcStringId.ALLIGATOR_ISLAND_LV_40);
 				htmltext = event;
 				break;
 			}
-			case "33861-03.html":
+			case "33859-07.html":
 			{
-				if (st.isCond(1))
+				if (st.isCond(2))
 				{
-					st.setCond(2, true);
-					takeItems(player, CARD, -1);
-					giveItems(player, EXP_MATERTIAL, 1);
+					st.setCond(3, true);
 					htmltext = event;
 				}
 				break;
 			}
-			case "33859-04.html":
+			case "33858-04.html":
 			{
-				if (st.isCond(2))
+				if (st.isCond(3))
 				{
 					st.exitQuest(false, true);
-					giveItems(player, STEEL_COIN, 1);
-					giveItems(player, EAC, 1);
-					addExpAndSp(player, 388290, 93);
+					giveItems(player, EAC, 5);
+					giveItems(player, STEEL_COIN, 17);
+					addExpAndSp(player, 2329740, 559);
+					htmltext = event;
 				}
 				break;
 			}
@@ -124,50 +131,47 @@ public final class Q10391_ASuspiciousHelper extends Quest
 		{
 			case State.CREATED:
 			{
-				if (npc.getId() == ELI)
+				if (npc.getId() == IASON)
 				{
-					htmltext = "33858-01.htm";
+					htmltext = "33859-01.htm";
 				}
 				break;
 			}
 			case State.STARTED:
 			{
-				switch (npc.getId())
+				if (npc.getId() == IASON)
 				{
-					case ELI:
+					switch (st.getCond())
 					{
-						if (st.isCond(1))
+						case 1:
 						{
-							htmltext = "33858-05.html";
+							htmltext = "33859-05.html";
+							break;
 						}
-						break;
+						case 2:
+						{
+							htmltext = "33859-06.html";
+							break;
+						}
+						case 3:
+						{
+							htmltext = "33859-08.html";
+							break;
+						}
 					}
-					case CHEL:
+				}
+				else if (npc.getId() == ELI)
+				{
+					if (st.isCond(3))
 					{
-						if (st.isCond(1))
-						{
-							htmltext = "33861-01.html";
-						}
-						else if (st.isCond(2))
-						{
-							htmltext = "33861-04.html";
-						}
-						break;
-					}
-					case IASON:
-					{
-						if (st.isCond(2))
-						{
-							htmltext = "33859-01.html";
-						}
-						break;
+						htmltext = "33858-01.html";
 					}
 				}
 				break;
 			}
 			case State.COMPLETED:
 			{
-				if (npc.getId() == ELI)
+				if (npc.getId() == IASON)
 				{
 					htmltext = getAlreadyCompletedMsg(player);
 				}
@@ -175,5 +179,20 @@ public final class Q10391_ASuspiciousHelper extends Quest
 			}
 		}
 		return htmltext;
+	}
+	
+	@Override
+	public String onKill(Npc npc, PlayerInstance killer, boolean isSummon)
+	{
+		final QuestState st = getQuestState(killer, false);
+		
+		if ((st != null) && st.isStarted() && st.isCond(1))
+		{
+			if (giveItemRandomly(killer, npc, FRAGMENT, 1, 4, 30, 0.8, true))
+			{
+				st.setCond(2);
+			}
+		}
+		return super.onKill(npc, killer, isSummon);
 	}
 }
--- l2junity-gameserver/dist/game/data/scripts/quests/QuestMasterHandler.java
+++ l2junity-gameserver/dist/game/data/scripts/quests/QuestMasterHandler.java
@@ -213,6 +213,7 @@ import quests.Q10366_RuinsStatusUpdate.Q10366_RuinsStatusUpdate;
 import quests.Q10368_RebellionOfMonsters.Q10368_RebellionOfMonsters;
 import quests.Q10390_KekropusLetter.Q10390_KekropusLetter;
 import quests.Q10391_ASuspiciousHelper.Q10391_ASuspiciousHelper;
+import quests.Q10392_FailureAndItsConsequences.Q10392_FailureAndItsConsequences;
 import quests.Q10501_ZakenEmbroideredSoulCloak.Q10501_ZakenEmbroideredSoulCloak;
 import quests.Q10502_FreyaEmbroideredSoulCloak.Q10502_FreyaEmbroideredSoulCloak;
 import quests.Q10503_FrintezzaEmbroideredSoulCloak.Q10503_FrintezzaEmbroideredSoulCloak;
@@ -435,6 +436,7 @@ public class QuestMasterHandler
 		Q10368_RebellionOfMonsters.class,
 		Q10390_KekropusLetter.class,
 		Q10391_ASuspiciousHelper.class,
+		Q10392_FailureAndItsConsequences.class,
 		Q10501_ZakenEmbroideredSoulCloak.class,
 		Q10502_FreyaEmbroideredSoulCloak.class,
 		Q10503_FrintezzaEmbroideredSoulCloak.class,
