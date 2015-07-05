4fc8ceb74ea79bb3b8ffadc9538abb193e6f9cb1
Sdw- <jeremy.garreau.pro@gmail.com>
2015-07-05 15:19:56 +0200

    With the effect handler :o

---
+++ l2junity-gameserver/dist/game/data/scripts/handlers/effecthandlers/Feed.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (C) 2004-2015 L2J Server
+ * 
+ * This file is part of L2J Server.
+ * 
+ * L2J Server is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * L2J Server is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package handlers.effecthandlers;
+
+import org.l2junity.Config;
+import org.l2junity.gameserver.enums.MountType;
+import org.l2junity.gameserver.model.StatsSet;
+import org.l2junity.gameserver.model.actor.instance.L2PetInstance;
+import org.l2junity.gameserver.model.actor.instance.PlayerInstance;
+import org.l2junity.gameserver.model.conditions.Condition;
+import org.l2junity.gameserver.model.effects.AbstractEffect;
+import org.l2junity.gameserver.model.skills.BuffInfo;
+
+/**
+ * @author Sdw
+ */
+public class Feed extends AbstractEffect
+{
+	private final int _normal;
+	private final int _ride;
+	private final int _wyvern;
+	
+	protected Feed(Condition attachCond, Condition applyCond, StatsSet set, StatsSet params)
+	{
+		super(attachCond, applyCond, set, params);
+		
+		_normal = params.getInt("normal", 0);
+		_ride = params.getInt("ride", 0);
+		_wyvern = params.getInt("wyvern", 0);
+	}
+	
+	@Override
+	public boolean isInstant()
+	{
+		return true;
+	}
+	
+	@Override
+	public void onStart(BuffInfo info)
+	{
+		if (info.getEffected().isPet())
+		{
+			final L2PetInstance pet = (L2PetInstance) info.getEffected();
+			pet.setCurrentFed(pet.getCurrentFed() + (_normal * Config.PET_FOOD_RATE));
+		}
+		else if (info.getEffected().isPlayer())
+		{
+			PlayerInstance player = info.getEffected().getActingPlayer();
+			if (player.getMountType() == MountType.WYVERN)
+			{
+				player.setCurrentFeed(player.getCurrentFeed() + _wyvern);
+			}
+			else
+			{
+				player.setCurrentFeed(player.getCurrentFeed() + _ride);
+			}
+		}
+	}
+}
