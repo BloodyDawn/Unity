e716ecbb805911e0922fe24228f2f6c2ec8977b3
NosBit <NosBit@users.noreply.github.com>
2015-07-05 15:38:29 +0300

    Removed unused field from Skill class.

--- l2junity-gameserver/src/main/java/org/l2junity/gameserver/model/skills/Skill.java
+++ l2junity-gameserver/src/main/java/org/l2junity/gameserver/model/skills/Skill.java
@@ -193,8 +193,6 @@ public final class Skill implements IIdentifiable
 	
 	private final boolean _isDebuff;
 	
-	private final String _attribute;
-	
 	private final boolean _isSuicideAttack;
 	private final boolean _canBeDispelled;
 	
@@ -267,9 +265,7 @@ public final class Skill implements IIdentifiable
 		_abnormalTime = abnormalTime;
 		_isAbnormalInstant = set.getBoolean("abnormalInstant", false);
 		parseAbnormalVisualEffect(set.getString("abnormalVisualEffect", null));
-		
-		_attribute = set.getString("attribute", "");
-		
+
 		_stayAfterDeath = set.getBoolean("stayAfterDeath", false);
 		_stayOnSubclassChange = set.getBoolean("stayOnSubclassChange", true);
 		
@@ -1621,11 +1617,7 @@ public final class Skill implements IIdentifiable
 	{
 		return _refId;
 	}
-	
-	public String getAttributeName()
-	{
-		return _attribute;
-	}
+
 
 	public boolean canBeDispelled()
 	{
