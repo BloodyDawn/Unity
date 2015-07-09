51d2e58123f66ceebab6180366963894b16b0a09
malyelfik <malyelfiiik@gmail.com>
2015-07-09 20:00:25 +0200

    Quest Waiting for the Summer (474)

--- l2junity-gameserver/dist/game/data/scripts/quests/QuestMasterHandler.java
+++ l2junity-gameserver/dist/game/data/scripts/quests/QuestMasterHandler.java
@@ -124,6 +124,7 @@ import quests.Q00458_PerfectForm.Q00458_PerfectForm;
 import quests.Q00463_IMustBeaGenius.Q00463_IMustBeaGenius;
 import quests.Q00464_Oath.Q00464_Oath;
 import quests.Q00470_DivinityProtector.Q00470_DivinityProtector;
+import quests.Q00474_WaitingForTheSummer.Q00474_WaitingForTheSummer;
 import quests.Q00481_ShadowHelper.Q00481_ShadowHelper;
 import quests.Q00490_DutyOfTheSurvivor.Q00490_DutyOfTheSurvivor;
 import quests.Q00501_ProofOfClanAlliance.Q00501_ProofOfClanAlliance;
@@ -395,6 +396,7 @@ public class QuestMasterHandler
 		Q00463_IMustBeaGenius.class,
 		Q00464_Oath.class,
 		Q00470_DivinityProtector.class,
+		Q00474_WaitingForTheSummer.class,
 		Q00481_ShadowHelper.class,
 		Q00490_DutyOfTheSurvivor.class,
 		Q00501_ProofOfClanAlliance.class,
