92da720c1f761ff353a8b2477aa0803b109b2bc9
Sdw- <jeremy.garreau.pro@gmail.com>
2015-07-24 16:20:22 +0200

    Energy of Chaos + prepareBattle stuff for CoC

--- l2junity-gameserver/src/main/java/org/l2junity/gameserver/model/ceremonyofchaos/CeremonyOfChaosEvent.java
+++ l2junity-gameserver/src/main/java/org/l2junity/gameserver/model/ceremonyofchaos/CeremonyOfChaosEvent.java
@@ -23,6 +23,7 @@ import java.util.concurrent.ConcurrentHashMap;
 
 import org.l2junity.gameserver.instancemanager.CeremonyOfChaosManager;
 import org.l2junity.gameserver.instancemanager.InstanceManager;
+import org.l2junity.gameserver.model.actor.Summon;
 import org.l2junity.gameserver.model.actor.instance.L2MonsterInstance;
 import org.l2junity.gameserver.model.actor.instance.PlayerInstance;
 import org.l2junity.gameserver.model.entity.Instance;
@@ -32,6 +33,7 @@ import org.l2junity.gameserver.model.holders.SkillHolder;
 import org.l2junity.gameserver.network.client.send.NpcHtmlMessage;
 import org.l2junity.gameserver.network.client.send.ceremonyofchaos.ExCuriousHouseEnter;
 import org.l2junity.gameserver.network.client.send.ceremonyofchaos.ExCuriousHouseMemberList;
+import org.l2junity.gameserver.network.client.send.string.SystemMessageId;
 
 /**
  * @author UnAfraid
@@ -96,8 +98,61 @@ public class CeremonyOfChaosEvent extends AbstractEvent<CeremonyOfChaosMember>
 			// Load the html
 			msg.setFile(player.getHtmlPrefix(), "data/html/CeremonyOfChaos/started.htm");
 			
-			// Teleport player to the arena
-			player.teleToLocation(_instance.getSpawnLoc(), _instance.getId(), 200);
+			// Remove buffs
+			player.stopAllEffects();
+			
+			// Player shouldn't be able to move and is hidden
+			player.setIsImmobilized(true);
+			player.setInvisible(true);
+			
+			// Same goes for summon
+			player.getServitors().values().forEach(s ->
+			{
+				s.setInvisible(true);
+				s.setIsImmobilized(true);
+			});
+			
+			// If player is dead, revive it
+			if (player.isDead())
+			{
+				player.doRevive();
+			}
+			
+			// If player is sitting, stand up
+			if (player.isSitting())
+			{
+				player.standUp();
+			}
+			
+			// If player in party, leave it
+			if (player.isInParty())
+			{
+				player.leaveParty();
+			}
+			
+			// Cancel any started action
+			player.abortAttack();
+			player.abortCast();
+			player.stopMove(null);
+			player.setTarget(null);
+			
+			// Unsummon pet
+			final Summon pet = player.getPet();
+			if (pet != null)
+			{
+				pet.unSummon(player);
+			}
+			
+			// Unsummon agathion
+			if (player.getAgathionId() > 0)
+			{
+				player.setAgathionId(0);
+			}
+			
+			// Fully regen player
+			player.setCurrentHp(player.getMaxHp());
+			player.setCurrentMp(player.getMaxMp());
+			player.setCurrentCp(player.getMaxCp());
 			
 			// Apply the Energy of Chaos skill
 			for (SkillHolder holder : CeremonyOfChaosManager.getInstance().getVariables().getList(CeremonyOfChaosManager.BUFF_KEY, SkillHolder.class))
@@ -119,12 +174,31 @@ public class CeremonyOfChaosEvent extends AbstractEvent<CeremonyOfChaosMember>
 			{
 				player.addItem("CoC", holder, null, true);
 			}
+			
+			// Teleport player to the arena
+			player.teleToLocation(_instance.getSpawnLoc(), _instance.getId(), 200);
 		}
 	}
 	
 	public void startFight()
 	{
-		
+		for (CeremonyOfChaosMember member : getMembers())
+		{
+			final PlayerInstance player = member.getPlayer();
+			if (player != null)
+			{
+				player.sendPacket(SystemMessageId.THE_MATCH_HAS_STARTED_FIGHT);
+				player.setIsImmobilized(false);
+				player.setInvisible(false);
+				player.broadcastInfo();
+				player.getServitors().values().forEach(s ->
+				{
+					s.setInvisible(false);
+					s.setIsImmobilized(false);
+					s.broadcastInfo();
+				});
+			}
+		}
 	}
 	
 	public void stopFight()
