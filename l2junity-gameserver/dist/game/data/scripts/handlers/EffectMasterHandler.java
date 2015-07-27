383584ed55082c0a8509c4b2be4a9d05b51f4f07
Sdw- <jeremy.garreau.pro@gmail.com>
2015-07-27 18:54:05 +0200

    Misc skills fix

--- l2junity-gameserver/dist/game/data/scripts/handlers/EffectMasterHandler.java
+++ l2junity-gameserver/dist/game/data/scripts/handlers/EffectMasterHandler.java
@@ -60,7 +60,6 @@ public final class EffectMasterHandler
 		ChangeFishingMastery.class,
 		ChangeHairColor.class,
 		ChangeHairStyle.class,
-		CheckForceSkill.class,
 		CheckSynergySkill.class,
 		ClanGate.class,
 		ClassChange.class,
