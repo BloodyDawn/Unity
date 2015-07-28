eaab9439fd058fa59b6fdf2200f343476a473049
Sdw- <jeremy.garreau.pro@gmail.com>
2015-07-28 09:47:06 +0200

    Never happened

---
+++ l2junity-gameserver/dist/game/data/scripts/handlers/effecthandlers/MagicalAttackRange.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright (C) 2004-2015 L2J DataPack
+ * 
+ * This file is part of L2J DataPack.
+ * 
+ * L2J DataPack is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * L2J DataPack is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package handlers.effecthandlers;
+
+import org.l2junity.commons.util.CommonUtil;
+import org.l2junity.commons.util.Rnd;
+import org.l2junity.gameserver.enums.ShotType;
+import org.l2junity.gameserver.model.StatsSet;
+import org.l2junity.gameserver.model.actor.Creature;
+import org.l2junity.gameserver.model.conditions.Condition;
+import org.l2junity.gameserver.model.effects.AbstractEffect;
+import org.l2junity.gameserver.model.effects.L2EffectType;
+import org.l2junity.gameserver.model.skills.BuffInfo;
+import org.l2junity.gameserver.model.stats.Formulas;
+import org.l2junity.gameserver.model.stats.Stats;
+
+/**
+ * Magical Attack effect implementation.
+ * @author Adry_85
+ */
+public final class MagicalAttackRange extends AbstractEffect
+{
+	private final double _power;
+	private final double _powerRange;
+	
+	public MagicalAttackRange(Condition attachCond, Condition applyCond, StatsSet set, StatsSet params)
+	{
+		super(attachCond, applyCond, set, params);
+		
+		_power = params.getDouble("power", 0);
+		_powerRange = params.getDouble("powerRange", 0);
+	}
+	
+	@Override
+	public L2EffectType getEffectType()
+	{
+		return L2EffectType.MAGICAL_ATTACK;
+	}
+	
+	@Override
+	public boolean isInstant()
+	{
+		return true;
+	}
+	
+	@Override
+	public void onStart(BuffInfo info)
+	{
+		Creature target = info.getEffected();
+		Creature activeChar = info.getEffector();
+		
+		if (target.isPlayer() && target.getActingPlayer().isFakeDeath())
+		{
+			target.stopFakeDeath(true);
+		}
+		
+		final double power = _power + CommonUtil.map(activeChar.calculateDistance(target, false, false), 0, info.getSkill().getCastRange(), _powerRange, 0);
+		
+		boolean sps = info.getSkill().useSpiritShot() && activeChar.isChargedShot(ShotType.SPIRITSHOTS);
+		boolean bss = info.getSkill().useSpiritShot() && activeChar.isChargedShot(ShotType.BLESSED_SPIRITSHOTS);
+		final boolean mcrit = Formulas.calcMCrit(activeChar.getMCriticalHit(target, info.getSkill()), info.getSkill(), target);
+		final byte shld = Formulas.calcShldUse(activeChar, target, info.getSkill());
+		int damage = (int) Formulas.calcMagicDam(activeChar, target, info.getSkill(), power, shld, sps, bss, mcrit);
+		
+		if (damage > 0)
+		{
+			// Manage attack or cast break of the target (calculating rate, sending message...)
+			if (!target.isRaid() && Formulas.calcAtkBreak(target, damage))
+			{
+				target.breakAttack();
+				target.breakCast();
+			}
+			
+			// Shield Deflect Magic: Reflect all damage on caster.
+			if (target.getStat().calcStat(Stats.VENGEANCE_SKILL_MAGIC_DAMAGE, 0, target, info.getSkill()) > Rnd.get(100))
+			{
+				activeChar.reduceCurrentHp(damage, target, info.getSkill());
+				activeChar.notifyDamageReceived(damage, target, info.getSkill(), mcrit, false, true);
+			}
+			else
+			{
+				target.reduceCurrentHp(damage, activeChar, info.getSkill());
+				target.notifyDamageReceived(damage, activeChar, info.getSkill(), mcrit, false, false);
+				activeChar.sendDamageMessage(target, damage, mcrit, false, false);
+			}
+		}
+		
+		if (info.getSkill().isSuicideAttack())
+		{
+			activeChar.doDie(activeChar);
+		}
+	}
+}
