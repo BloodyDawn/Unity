8e64a8ca39d6bc6cd906bafdaf1f6e66a69ade5b
Nik <nik_cro@abv.bg>
2015-08-03 01:44:01 +0300

    0.05 activateRate fixed so the server can load. Small update on BasicProperty enum comment so it can be easier to navigate which basicProperty to set when making new skills.

--- l2junity-gameserver/src/main/java/org/l2junity/gameserver/model/stats/BasicProperty.java
+++ l2junity-gameserver/src/main/java/org/l2junity/gameserver/model/stats/BasicProperty.java
@@ -25,6 +25,13 @@ import org.l2junity.gameserver.model.actor.Creature;
  * Before Goddess of Destruction, BaseStats was used. CON for physical, MEN for magical, and others for special cases. <BR>
  * After, only 3 types are used: physical, magic and none. <BR>
  * <BR>
+ * Quote from Juji: <BR>
+ * ---------------------------------------------------------------------- <BR>
+ * Physical: Stun, Paralyze, Knockback, Knock Down, Hold, Disarm, Petrify <BR>
+ * Mental: Sleep, Mutate, Fear, Aerial Yoke, Silence <BR>
+ * ---------------------------------------------------------------------- <BR>
+ * All other are considered with no basic property aka NONE. <BR>
+ * <BR>
  * @author Nik
  */
 public enum BasicProperty
