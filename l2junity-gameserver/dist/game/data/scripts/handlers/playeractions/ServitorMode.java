dd5c74b0a07706f3bf973cf3bc5312a01acd87ce
Nik <nik_cro@abv.bg>
2015-08-15 19:15:20 +0300

    PetActions and SummonActions splitted.

--- l2junity-gameserver/dist/game/data/scripts/handlers/playeractions/ServitorMode.java
+++ l2junity-gameserver/dist/game/data/scripts/handlers/playeractions/ServitorMode.java
@@ -18,78 +18,62 @@
  */
 package handlers.playeractions;
 
-import org.l2junity.gameserver.ai.CtrlIntention;
 import org.l2junity.gameserver.ai.SummonAI;
 import org.l2junity.gameserver.handler.IPlayerActionHandler;
 import org.l2junity.gameserver.handler.PlayerActionHandler;
 import org.l2junity.gameserver.model.ActionDataHolder;
-import org.l2junity.gameserver.model.actor.instance.L2PetInstance;
 import org.l2junity.gameserver.model.actor.instance.PlayerInstance;
 import org.l2junity.gameserver.network.client.send.string.SystemMessageId;
 
 /**
- * Pet actions player action handler.
+ * Servitor passive/defend mode player action handler.
  * @author Nik
  */
-public final class PetAction implements IPlayerActionHandler
+public final class ServitorMode implements IPlayerActionHandler
 {
 	@Override
 	public void useAction(PlayerInstance activeChar, ActionDataHolder data, boolean ctrlPressed, boolean shiftPressed)
 	{
-		
-		if ((activeChar.getPet() == null) || !activeChar.getPet().isPet())
+		if (!activeChar.hasServitors())
 		{
-			activeChar.sendPacket(SystemMessageId.YOU_DO_NOT_HAVE_A_PET);
-			return;
-		}
-		
-		L2PetInstance pet = (L2PetInstance) activeChar.getPet();
-		if (pet.isUncontrollable())
-		{
-			activeChar.sendPacket(SystemMessageId.WHEN_YOUR_PET_S_HUNGER_GAUGE_IS_AT_0_YOU_CANNOT_USE_YOUR_PET);
-			return;
-		}
-		
-		if (pet.isBetrayed())
-		{
-			activeChar.sendPacket(SystemMessageId.YOUR_PET_SERVITOR_IS_UNRESPONSIVE_AND_WILL_NOT_OBEY_ANY_ORDERS);
+			activeChar.sendPacket(SystemMessageId.YOU_DO_NOT_HAVE_A_SERVITOR);
 			return;
 		}
 		
 		switch (data.getOptionId())
 		{
-			case 1: // Attack
+			case 1: // Passive mode
 			{
-				if (pet.canAttack(activeChar.getTarget(), ctrlPressed))
+				activeChar.getServitors().values().forEach(s ->
 				{
-					pet.doAttack(activeChar.getTarget());
-				}
+					if (s.isBetrayed())
+					{
+						activeChar.sendPacket(SystemMessageId.YOUR_PET_SERVITOR_IS_UNRESPONSIVE_AND_WILL_NOT_OBEY_ANY_ORDERS);
+						return;
+					}
+					
+					((SummonAI) s.getAI()).setDefending(false);
+				});
 				break;
 			}
-			case 2: // Stop
+			case 2: // Defending mode
 			{
-				pet.cancelAction();
-				break;
-			}
-			case 3: // Change Movement Mode
-			{
-				((SummonAI) pet.getAI()).notifyFollowStatusChange();
-				break;
-			}
-			case 4: // Move to target
-			{
-				if ((activeChar.getTarget() != null) && (pet != activeChar.getTarget()) && !pet.isMovementDisabled())
+				activeChar.getServitors().values().forEach(s ->
 				{
-					pet.setFollowStatus(false);
-					pet.getAI().setIntention(CtrlIntention.AI_INTENTION_MOVE_TO, activeChar.getTarget().getLocation());
-				}
+					if (s.isBetrayed())
+					{
+						activeChar.sendPacket(SystemMessageId.YOUR_PET_SERVITOR_IS_UNRESPONSIVE_AND_WILL_NOT_OBEY_ANY_ORDERS);
+						return;
+					}
+					
+					((SummonAI) s.getAI()).setDefending(true);
+				});
 			}
 		}
-		
 	}
 	
 	public static void main(String[] args)
 	{
-		PlayerActionHandler.getInstance().registerHandler(new PetAction());
+		PlayerActionHandler.getInstance().registerHandler(new ServitorMode());
 	}
 }
