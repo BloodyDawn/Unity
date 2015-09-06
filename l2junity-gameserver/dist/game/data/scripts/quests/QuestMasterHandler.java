816f3bf5d9508435f81214471187fcd0a9e0a07f
St3eT <St3eT@seznam.cz>
2015-09-06 13:24:17 +0200

    Quest A Daunting Task (763).

--- l2junity-gameserver/dist/game/data/scripts/quests/QuestMasterHandler.java
+++ l2junity-gameserver/dist/game/data/scripts/quests/QuestMasterHandler.java
@@ -164,6 +164,7 @@ import quests.Q00701_ProofOfExistence.Q00701_ProofOfExistence;
 import quests.Q00702_ATrapForRevenge.Q00702_ATrapForRevenge;
 import quests.Q00761_AssistingTheGoldenRamArmy.Q00761_AssistingTheGoldenRamArmy;
 import quests.Q00762_AnOminousRequest.Q00762_AnOminousRequest;
+import quests.Q00763_ADauntingTask.Q00763_ADauntingTask;
 import quests.Q00901_HowLavasaurusesAreMade.Q00901_HowLavasaurusesAreMade;
 import quests.Q00902_ReclaimOurEra.Q00902_ReclaimOurEra;
 import quests.Q00903_TheCallOfAntharas.Q00903_TheCallOfAntharas;
@@ -451,6 +452,7 @@ public class QuestMasterHandler
 		Q00702_ATrapForRevenge.class,
 		Q00761_AssistingTheGoldenRamArmy.class,
 		Q00762_AnOminousRequest.class,
+		Q00763_ADauntingTask.class,
 		Q00901_HowLavasaurusesAreMade.class,
 		Q00902_ReclaimOurEra.class,
 		Q00903_TheCallOfAntharas.class,
