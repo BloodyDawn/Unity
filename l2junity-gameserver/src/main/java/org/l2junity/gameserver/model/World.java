501eaf120f7f90a57fc95d180dfda6f2ce0aeb9c
Sdw- <jeremy.garreau.pro@gmail.com>
2015-10-28 14:25:42 +0100

    Fixing gitignore, thanks @malyelfik

--- l2junity-gameserver/src/main/java/org/l2junity/gameserver/model/World.java
+++ l2junity-gameserver/src/main/java/org/l2junity/gameserver/model/World.java
@@ -509,7 +509,7 @@ public final class World
 			{
 				for (WorldObject wo : w.getVisibleObjects().values())
 				{
-					if ((wo == object) || (wo.getInstanceWorld() != object.getInstanceWorld()))
+					if (wo == object)
 					{
 						continue;
 					}
