b79bf7e8f67fff99b6975f037481060f8e721c56
St3eT <St3eT@seznam.cz>
2015-11-03 01:37:31 +0100

    Fak :O

--- l2junity-gameserver/dist/game/data/scripts/quests/ThirdClassTransferQuest.java
+++ l2junity-gameserver/dist/game/data/scripts/quests/ThirdClassTransferQuest.java
@@ -215,7 +215,7 @@ public abstract class ThirdClassTransferQuest extends Quest
 						break;
 					}
 					
-					npc.doCast(SHOW_SKILL.getSkill());
+					addSkillCastDesire(npc, player, SHOW_SKILL.getSkill(), 23);
 					player.sendPacket(SystemMessageId.CONGRATULATIONS_YOU_VE_COMPLETED_YOUR_THIRD_CLASS_TRANSFER_QUEST);
 					player.broadcastSocialAction(3);
 					player.setBaseClass(newClassId);
