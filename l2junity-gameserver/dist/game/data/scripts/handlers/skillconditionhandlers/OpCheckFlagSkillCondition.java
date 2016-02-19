681c5bdbb1d8d6ff8350d6c8f837c85857e9fea0
Sdw- <jeremy.garreau.pro@gmail.com>
2016-02-19 15:29:08 +0100

    Misc

--- l2junity-gameserver/dist/game/data/scripts/handlers/skillconditionhandlers/OpCheckFlagSkillCondition.java
+++ l2junity-gameserver/dist/game/data/scripts/handlers/skillconditionhandlers/OpCheckFlagSkillCondition.java
@@ -18,14 +18,20 @@
  */
 package handlers.skillconditionhandlers;
 
+import org.l2junity.gameserver.instancemanager.FortManager;
 import org.l2junity.gameserver.model.StatsSet;
 import org.l2junity.gameserver.model.WorldObject;
 import org.l2junity.gameserver.model.actor.Creature;
+import org.l2junity.gameserver.model.actor.instance.PlayerInstance;
+import org.l2junity.gameserver.model.entity.Fort;
 import org.l2junity.gameserver.model.skills.ISkillCondition;
 import org.l2junity.gameserver.model.skills.Skill;
+import org.l2junity.gameserver.network.client.send.SystemMessage;
+import org.l2junity.gameserver.network.client.send.string.SystemMessageId;
+import org.l2junity.gameserver.util.Util;
 
 /**
- * @author
+ * @author Sdw
  */
 public class OpCheckFlagSkillCondition implements ISkillCondition
 {
@@ -37,6 +43,37 @@ public class OpCheckFlagSkillCondition implements ISkillCondition
 	@Override
 	public boolean canUse(Creature caster, Skill skill, WorldObject target)
 	{
-		return false;
+		if (!caster.isPlayer())
+		{
+			return false;
+		}
+		
+		final PlayerInstance player = caster.getActingPlayer();
+		boolean canTakeFort = true;
+		if (player.isAlikeDead() || player.isCursedWeaponEquipped() || !player.isClanLeader())
+		{
+			canTakeFort = false;
+		}
+		
+		final Fort fort = FortManager.getInstance().getFort(player);
+		final SystemMessage sm;
+		if ((fort == null) || (fort.getResidenceId() <= 0) || !fort.getSiege().isInProgress() || (fort.getSiege().getAttackerClan(player.getClan()) == null))
+		{
+			sm = SystemMessage.getSystemMessage(SystemMessageId.S1_CANNOT_BE_USED_DUE_TO_UNSUITABLE_TERMS);
+			sm.addSkillName(skill);
+			player.sendPacket(sm);
+			canTakeFort = false;
+		}
+		else if (fort.getFlagPole() != target)
+		{
+			player.sendPacket(SystemMessageId.INVALID_TARGET);
+			canTakeFort = false;
+		}
+		else if (!Util.checkIfInRange(200, player, target, true))
+		{
+			player.sendPacket(SystemMessageId.THE_DISTANCE_IS_TOO_FAR_AND_SO_THE_CASTING_HAS_BEEN_STOPPED);
+			canTakeFort = false;
+		}
+		return canTakeFort;
 	}
 }
