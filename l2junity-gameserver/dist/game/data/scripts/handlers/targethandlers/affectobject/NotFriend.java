14972cba5b80823b940aa15e6ab10b7653f3d83d
Nik <nik_cro@abv.bg>
2016-02-21 18:55:52 +0200

    Friend/NotFriend affect objects npc relation fixed.

--- l2junity-gameserver/dist/game/data/scripts/handlers/targethandlers/affectobject/NotFriend.java
+++ l2junity-gameserver/dist/game/data/scripts/handlers/targethandlers/affectobject/NotFriend.java
@@ -22,6 +22,9 @@ import org.l2junity.gameserver.handler.IAffectObjectHandler;
 import org.l2junity.gameserver.model.L2Clan;
 import org.l2junity.gameserver.model.Party;
 import org.l2junity.gameserver.model.actor.Creature;
+import org.l2junity.gameserver.model.actor.instance.L2DefenderInstance;
+import org.l2junity.gameserver.model.actor.instance.L2GuardInstance;
+import org.l2junity.gameserver.model.actor.instance.L2NpcInstance;
 import org.l2junity.gameserver.model.actor.instance.PlayerInstance;
 import org.l2junity.gameserver.model.skills.targets.AffectObject;
 import org.l2junity.gameserver.model.zone.ZoneId;
@@ -132,11 +135,19 @@ public class NotFriend implements IAffectObjectHandler
 		}
 		else if (activeChar.isMonster())
 		{
-			return !target.isMonster() && target.isAutoAttackable(activeChar);
+			// Monsters consider any other monsters or non-folk NPCs as friends
+			if (target.isMonster() || (target.isNpc() && !(target instanceof L2NpcInstance) && !(target instanceof L2GuardInstance) && !(target instanceof L2DefenderInstance)))
+			{
+				return false;
+			}
 		}
 		else if (activeChar.isNpc())
 		{
-			return target.isMonster() || target.isAutoAttackable(activeChar);
+			// Non-folk, non-monster aka neutral NPCs consider everything friendly.
+			if (!(activeChar instanceof L2NpcInstance) && !(activeChar instanceof L2GuardInstance) && !(activeChar instanceof L2DefenderInstance))
+			{
+				return false;
+			}
 		}
 		
 		return target.isAutoAttackable(activeChar);
